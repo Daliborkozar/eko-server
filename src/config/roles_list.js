1253fc568b05730d56eac9f7cfd0ff77587b92a1
Vasko Raketic <vasx93@hotmail.com>
2024-03-01 00:02:39 +0100

    fix roles

--- src/config/roles_list.js
+++ src/config/roles_list.js
@@ -1,3 +1,23 @@
 const ROLES_LIST = ['SuperAdmin', 'Admin', 'User', 'Patient'];
 
+// roles as in db
+const ROLES = [
+  {
+    name: 'SuperAdmin',
+    manages: ['SuperAdmin', 'Admin', 'User', 'Patient'],
+  },
+  {
+    name: 'Admin',
+    manages: ['User', 'Patient'],
+  },
+  {
+    name: 'User',
+    manages: ['User', 'Patient'],
+  },
+  {
+    name: 'Patient',
+    manages: [''],
+  },
+];
+
 module.exports = ROLES_LIST;
