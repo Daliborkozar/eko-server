290d306550bb151f854b8d89a9670718ad0f9e6e
Vasko Raketic <vasx93@hotmail.com>
2024-03-07 00:35:18 +0100

    fix auth routes, add get me route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { AuthController } = require('../controllers/auth');
+const verifyJWT = require('../middleware/verifyJWT');
 
 const router = express.Router();
 
+router.get('/me', verifyJWT, AuthController.me);
 router.post('/login', AuthController.handleLogin);
 router.post('/logout', AuthController.handleLogout);
 router.post('/refresh', AuthController.handleRefreshToken);
