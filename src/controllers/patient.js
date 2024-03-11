b20aeee74e9a43593adf831022e25ba3bd8ec115
Vasko Raketic <vasx93@hotmail.com>
2024-03-11 19:02:59 +0100

    create patient

--- src/controllers/patient.js
+++ src/controllers/patient.js
@@ -1,51 +1,21 @@
-const User = require('../model/user');
-const bcrypt = require('bcrypt');
+const { ROLES } = require('../config/roles_list');
+const Patient = require('../model/patient');
 
-const handleNewUser = async (req, res) => {
-  const { user, pwd, roles, organization, isActive, displayName, email } = req.body;
+const createPatient = async (req, res) => {
+  const { user } = req;
 
-  try {
-    // Check if the user making the request is a super admin
-    const requestingUser = req.user; // Assuming you have middleware to extract the user from the request
-    //if (requestingUser && requestingUser.roles && requestingUser.roles.SuperAdmin) {
-    // The user making the request is a super admin
+  const patientData = {
+    ...req.body,
+    admittedBy: user._id,
+    organization: user.organization,
+    role: ROLES.Patient.name,
+  };
 
-    // check for duplicate usernames in the db
-    const duplicateUser = await User.findOne({ username: user }).exec();
-    const duplicateEmail = await User.findOne({ email: email }).exec();
-    // if (duplicate) return res.sendStatus(409); // Conflict
-    if (duplicateUser) {
-      return res.status(409).json({ error: 'Username already exists' });
-    }
+  const patient = (await Patient.create(patientData)).toObject();
 
-    if (duplicateEmail) {
-      return res.status(409).json({ error: 'Email already exists' });
-    }
-    // encrypt the password
-    const hashedPwd = await bcrypt.hash(pwd, 10);
+  if (!patient) res.status(500).send({ message: 'Unable to create a patient' });
 
-    // create and store the new user
-    const result = await User.create({
-      username: user,
-      password: hashedPwd,
-      roles: roles,
-      displayName: displayName,
-      organization: organization,
-      isActive: isActive,
-      email: email,
-    });
-
-    console.log(result);
-
-    res.status(201).json({ success: `New user ${user} created!` });
-    //} else {
-    // The user making the request is not a super admin
-    //console.log('Registration failed. Insufficient privileges.');
-    //res.status(403).json({ error: 'Insufficient privileges' });
-    // }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  return res.status(201).send(patient);
 };
 
-module.exports = PatientController = { handleNewUser };
+module.exports = PatientController = { createPatient };
--- src/routes/patient.js
+++ src/routes/patient.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Patient = require('../controllers/patient');
+const verifyJWT = require('../middleware/verifyJWT');
+const verifyRoles = require('../middleware/verifyRoles');
 
 const router = express.Router();
 
-router.post('/', Patient.handleNewUser);
+router.post('/', verifyJWT, verifyRoles, Patient.createPatient);
 
 module.exports = router;
