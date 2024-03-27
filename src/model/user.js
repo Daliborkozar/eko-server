641a412e6c7050baf5ebd0f65ef6fa5ff4571b3b
Vasko Raketic <vasx93@hotmail.com>
2024-03-27 17:33:27 +0100

    add indice

--- src/model/user.js
+++ src/model/user.js
@@ -39,6 +39,9 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.index({ email: 1 });
+userSchema.index({ role: 1 });
+userSchema.index({ role: 1, organization: 1 });
 userSchema.index({ isActive: 1, role: 1 });
 userSchema.index({ isActive: 1, organization: 1 });
 
